8a67d149d0e522791a60616ca49c6dd57e6d1b86
Maria <marijka000@gmail.com>
2022-12-22 10:03:46 +0300

    Player New 2

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -7,8 +7,8 @@ public class Player {
     private String name;
 
     /** информация о том, в какую игру сколько часов было сыграно
-    ключ - игра
-    значение - суммарное количество часов игры в эту игру */
+     ключ - игра
+     значение - суммарное количество часов игры в эту игру */
     private Map<Game, Integer> playedTime = new HashMap<>();
 
     public Player(String name) {
@@ -20,22 +20,22 @@ public class Player {
     }
 
     /** добавление игры игроку
-    если игра уже была, никаких изменений происходить не должно */
+     если игра уже была, никаких изменений происходить не должно */
     public void installGame(Game game) {
         playedTime.put(game, 0);
     }
 
     /** игрок играет в игру game на протяжении hours часов
-    об этом нужно сообщить объекту-каталогу игр, откуда была установлена игра
-    также надо обновить значения в мапе игрока, добавив проигранное количество часов
-    возвращает суммарное количество часов, проигранное в эту игру.
-    если игра не была установлена, то надо выкидывать RuntimeException */
+     об этом нужно сообщить объекту-каталогу игр, откуда была установлена игра
+     также надо обновить значения в мапе игрока, добавив проигранное количество часов
+     возвращает суммарное количество часов, проигранное в эту игру.
+     если игра не была установлена, то надо выкидывать RuntimeException */
     public int play(Game game, int hours) {
         game.getStore().addPlayTime(name, hours);
         if (playedTime.containsKey(game)) {
-            playedTime.put(game, playedTime.get(game));
+            playedTime.put(game, playedTime.getOrDefault(game, hours) + hours);
         } else {
-            playedTime.put(game, hours);
+            throw new RuntimeException("Игра" + game.getTitle() + " не установлена");
         }
         return playedTime.get(game);
     }
@@ -47,8 +47,6 @@ public class Player {
         for (Game game : playedTime.keySet()) {
             if (game.getGenre().equals(genre)) {
                 sum += playedTime.get(game);
-            } else {
-                sum = 0;
             }
         }
         return sum;
--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,6 +1,8 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
@@ -19,5 +21,87 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
 
-    // другие ваши тесты
+    @Test
+    public void NameTest() {
+        Player player = new Player("Petya");
+        player.getName();
+
+        Assertions.assertEquals("Petya", player.getName());
+    }
+
+    @Test
+    public void CheckingPlaybackWithoutInstallation() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Баттл", "Аркады");
+
+        Player player = new Player("Alex");
+
+        assertThrows(RuntimeException.class, () -> player.play(game, 2));
+    }
+
+    @Test
+    public void thePlaybackCheckIsZero() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Баттл", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.play(game, 0);
+        player.play(game1, 0);
+
+        int expected = 0;
+        int actual = player.sumGenre(game.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkingAGameOfTheSameGenreForTheSumOfHours() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Баттл", "Аркады");
+
+        Player player = new Player("Alex");
+        player.installGame(game);
+        player.installGame(game1);
+        player.play(game, 2);
+        player.play(game1, 3);
+
+        int expected = 5;
+        int actual = player.sumGenre("Аркады");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkingAGameOfTheSameGenreForTheSumOfHours2() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Баттл", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.play(game, 0);
+        player.play(game1, 1);
+
+        int expected = 1;
+        int actual = player.sumGenre("Аркады");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkingAGameOfTheSameGenreForTheSumOfHours5() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("ММММММ", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.play(game, -1);
+
+        String expected = null;
+        Game actual = player.mostPlayerByGenre("Аркады");
+        assertEquals(expected, actual);
+    }
 }
